c5bccdea8166488d0b9548aa85eaac4378e91c98
Ramun McCallum <matthew@synapseware.com>
2019-12-07 10:19:54 -0800

    Fixing up PlatformIO stuffs

--- wifilogger.ino
+++ wifilogger.ino
@@ -1,9 +1,4 @@
-#include <util.h>
-#include <EthernetServer.h>
-#include <Ethernet2.h>
 #include <Dns.h>
-#include <EthernetClient.h>
-#include <EthernetUdp2.h>
 #include <Dhcp.h>
 #include <AdafruitIO_Time.h>
 #include <AdafruitIO_Definitions.h>
@@ -14,20 +9,16 @@
 #include <AdafruitIO_Group.h>
 #include <AdafruitIO.h>
 #include <AdafruitIO_WiFi.h>
-
-
-#include "headers/credentials.h"
-#include "headers/adafruitio.h"
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
+#include "headers/credentials.h"
+#include "headers/adafruitio.h"
 
 
-const char* ssid     = STASSID;
+const char* ssid		 = STASSID;
 const char* password = STAPSK;
 
-//const char* host = "djxmmx.net";
-//const uint16_t port = 17;
 
 const uint8_t SENSOR = D1;
 const uint8_t LED_PIN = D2;
@@ -53,38 +44,37 @@ AdafruitIO_Feed * temperatureFeed = io.feed("temperature");
 
 
 
-
 //-----------------------------------------------------------------------------
 // Configure hardware and initial state
 void setup()
 {
-  pinMode(LED_PIN, OUTPUT);
-  pinMode(ERR_PIN, OUTPUT);
+	pinMode(LED_PIN, OUTPUT);
+	pinMode(ERR_PIN, OUTPUT);
 
-  digitalWrite(LED_PIN, 0);
-  digitalWrite(ERR_PIN, 0);
+	digitalWrite(LED_PIN, 0);
+	digitalWrite(ERR_PIN, 0);
 
-  Serial.begin(115200);
-  delay(1000);
+	Serial.begin(115200);
+	delay(1000);
 
-  sprintf(msg, "WiFi info.  SSID=\"%s\", Password=\"%s\"", STASSID, STAPSK);
-  Serial.println(msg);
+	sprintf(msg, "WiFi info.	SSID=\"%s\", Password=\"%s\"", STASSID, STAPSK);
+	Serial.println(msg);
 
-  Serial.print("Connecting to AdafruitIO");
-  io.connect();
-  digitalWrite(ERR_PIN, 1);
-  while (io.status() < AIO_CONNECTED)
-  {
-    Serial.print(".");
-    delay(500);
-  }
-  digitalWrite(ERR_PIN, 0);
+	Serial.print("Connecting to AdafruitIO");
+	io.connect();
+	digitalWrite(ERR_PIN, 1);
+	while (io.status() < AIO_CONNECTED)
+	{
+		Serial.print(".");
+		delay(500);
+	}
+	digitalWrite(ERR_PIN, 0);
 
-  Serial.println();
-  Serial.println(io.statusText());
-  Serial.println();
+	Serial.println();
+	Serial.println(io.statusText());
+	Serial.println();
 
-  ReadSensorAddress();
+	ReadSensorAddress();
 }
 
 
@@ -92,14 +82,14 @@ void setup()
 // 
 void loop()
 {
-  io.run();
+	io.run();
 
-  float tempC = ReadTemperature(sensor);
-  float tempF = 1.8 * tempC + 32;
-  temperatureFeed->save(tempC);
-  sprintf(msg, "Temp: %f/C  %f/F\n", tempC, tempF);
-  Serial.print(msg);
-  delay(5000);
+	float tempC = ReadTemperature(sensor);
+	float tempF = 1.8 * tempC + 32;
+	temperatureFeed->save(tempC);
+	sprintf(msg, "Temp: %f/C	%f/F\n", tempC, tempF);
+	Serial.print(msg);
+	delay(5000);
 }
 
 
@@ -107,12 +97,12 @@ void loop()
 // 
 float ReadTemperature(DeviceAddress device)
 {
-  digitalWrite(LED_PIN, HIGH);
-  sensors.requestTemperatures();
-  float temperature = sensors.getTempC(device);
-  digitalWrite(LED_PIN, LOW);
+	digitalWrite(LED_PIN, HIGH);
+	sensors.requestTemperatures();
+	float temperature = sensors.getTempC(device);
+	digitalWrite(LED_PIN, LOW);
 
-  return temperature;
+	return temperature;
 }
 
 
@@ -120,22 +110,25 @@ float ReadTemperature(DeviceAddress device)
 // Reads the sensors address(es) and prints them to the console
 void ReadSensorAddress(void)
 {
-  byte i;
-  byte addr[8];
-  
-  Serial.println("Discovering temperature sensors...");
-  if (!ds.search(addr)) {
-    Serial.println("No more addresses.");
-    Serial.println();
-    ds.reset_search();
-    delay(250);
-    return;
-  }
-  Serial.print(" ROM: ");
-  for (i = 0; i < 8; i++) {
-    sprintf(msg + i * 6, "0x%02x, ", addr[i]);
-  }
-  int len = strlen(msg);
-  msg[len-2] = '\0';
-  Serial.println(msg);
+	byte i;
+	byte addr[8];
+	
+	Serial.println("Discovering temperature sensors...");
+
+	if (!ds.search(addr))
+  {
+		Serial.println("No more addresses.");
+		Serial.println();
+		ds.reset_search();
+		delay(250);
+		return;
+	}
+
+	Serial.print(" ROM: ");
+	for (i = 0; i < 8; i++) {
+		sprintf(msg + i * 6, "0x%02x, ", addr[i]);
+	}
+	int len = strlen(msg);
+	msg[len-2] = '\0';
+	Serial.println(msg);
 }
